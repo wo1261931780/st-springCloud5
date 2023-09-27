730a37d13e44a7f179eeb17d8f3986d45322b3c0
junw <wo1261931780@gmail.com>
2023-09-27 11:00:27 +0800

    初始化

---
+++ src/main/java/junw/stspringCloud3/StSpringCloud3Application.java
@@ -0,0 +1,13 @@
+package junw.stspringCloud3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StSpringCloud3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StSpringCloud3Application.class, args);
+	}
+
+}
