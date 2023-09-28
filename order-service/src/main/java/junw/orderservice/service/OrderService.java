e49414540b933644fbc51f8ba8fff9bd0ce501ff
junw <wo1261931780@gmail.com>
2023-09-28 11:12:05 +0800

    day001-4

--- order-service/src/main/java/junw/orderservice/service/OrderService.java
+++ order-service/src/main/java/junw/orderservice/service/OrderService.java
@@ -12,27 +12,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderService {
 
-    @Autowired
-    private OrderMapper orderMapper;
+	@Autowired
+	private OrderMapper orderMapper;
 
-    @Autowired
-    private UserClient userClient;
+	@Autowired
+	private UserClient userClient;
 
-    public Order queryOrderById(Long orderId) {
-        // 1.查询订单
-        Order order = orderMapper.findById(orderId);
-        // 2.用Feign远程调用
-        User user = userClient.findById(order.getUserId());
-        // 3.封装user到Order
-        order.setUser(user);
-        // 4.返回
-        return order;
-    }
+	public Order queryOrderById(Long orderId) {
+		// 1.查询订单
+		Order order = orderMapper.findById(orderId);
+		// 2.用Feign远程调用
+		User user = userClient.findById(order.getUserId());
+		// 3.封装user到Order
+		order.setUser(user);
+		// 4.返回
+		return order;
+	}
 
-    @SentinelResource("goods")
-    public void queryGoods(){
-        System.err.println("查询商品");
-    }
+	@SentinelResource("goods")
+	public void queryGoods() {
+		System.err.println("查询商品");
+	}
+	// 使用特殊注解，将其标注为一个资源
 
     /*@Autowired
     private RestTemplate restTemplate;
--- order-service/src/main/java/junw/orderservice/web/OrderController.java
+++ order-service/src/main/java/junw/orderservice/web/OrderController.java
@@ -13,37 +13,53 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("order")
 public class OrderController {
+	// 每个暴露出去的接口，都是链路中的一部分
 
-    @Autowired
-    private OrderService orderService;
+	@Autowired
+	private OrderService orderService;
 
-    @SentinelResource("hot")
-    @GetMapping("{orderId}")
-    public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
-        // 根据id查询订单并返回
-        return orderService.queryOrderById(orderId);
-    }
+	@SentinelResource("hot")
+	@GetMapping("{orderId}")
+	public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
+		// 根据id查询订单并返回
+		return orderService.queryOrderById(orderId);
+	}
+	// 这里已经被监控了，但是service还没有被监控
+	// 单机阈值1，就是每秒最多一个请求
+	// 超出的部分会直接报错
 
-    @GetMapping("/query")
-    public String queryOrder() {
-        // 查询商品
-        orderService.queryGoods();
-        // 查询订单
-        System.err.println("查询订单");
-        return "查询订单成功";
-    }
+	// SentinelResource将其标注为一个资源
+	
 
-    @GetMapping("/save")
-    public String saveOrder() {
-        // 查询商品
-        orderService.queryGoods();
-        // 查询订单
-        System.err.println("新增订单");
-        return "新增订单成功";
-    }
+	@GetMapping("/query")
+	public String queryOrder() {
+		// 查询商品
+		orderService.queryGoods();
+		// 查询订单
+		System.err.println("查询订单");
+		return "查询订单成功";
+	}
+	// 关联限制，根据优先级来判断，query阻塞的时候
+	// save也会被阻塞，因为save的优先级比较低
+	// 在链路模式中，默认只会监控Controller中的端点
+// service中不会收到影响
+// 而因为没有被监控，所以不会收到限流规则的影响
 
-    @GetMapping("/update")
-    public String updateOrder() {
-        return "更新订单成功";
-    }
+
+
+
+
+	@GetMapping("/save")
+	public String saveOrder() {
+		// 查询商品
+		orderService.queryGoods();
+		// 查询订单
+		System.err.println("新增订单");
+		return "新增订单成功";
+	}
+
+	@GetMapping("/update")
+	public String updateOrder() {
+		return "更新订单成功";
+	}
 }
